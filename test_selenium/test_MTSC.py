2782eeaf59e190930a22d1eeae42d21595a658a1
chloehan <1160147043@qq.com>
2020-03-22 22:28:37 +0800

    selenium-多浏览器学习

--- test_selenium/test_MTSC.py
+++ test_selenium/test_MTSC.py
@@ -3,6 +3,7 @@
 # @Time  : 2020/3/14 9:40 下午
 # @Author: chloehan
 # @File  : test_MTSC.py
+import os
 import time
 
 import pytest
@@ -12,15 +13,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 
+
 class TestMTSC:
     def setup_method(self):
-        self.driver = webdriver.Chrome()
-        # self.driver.get('https://testerhome.com')
+        # 多浏览器
+        browser = os.getenv("browser").lower()
+        if browser == "headless":
+            self.driver = webdriver.PhantomJS()
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "firefox":
+            self.driver = webdriver.Firefox()
+        # self.driver = webdriver.Chrome()
+        self.driver.get('https://testerhome.com')
         self.driver.implicitly_wait(5)
 
     def test_page(self):
         self.driver.find_element(By.CSS_SELECTOR, '[title="MTSC2020 中国互联网测试开发大会议题征集"]').click()
-        element = (By.CSS_SELECTOR, '.toc-container.dropdown')
+        element = (By.CSS_SELECTO, '.toc-container.dropdown')
         WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable(element))
         self.driver.find_element(*element).click()
         self.driver.find_element(By.CSS_SELECTOR, '.toc-item:nth-child(4) > .toc-item-link').click()
@@ -34,13 +44,11 @@ class TestMTSC:
 
     def test_mtsc2020(self):
         self.driver.get("https://testerhome.com/topics/21805")
-        self.driver.find_element(By.LINK_TEXT,'第六届中国互联网测试开发大会').click()
+        self.driver.find_element(By.LINK_TEXT, '第六届中国互联网测试开发大会').click()
         print(self.driver.window_handles)
         self.driver.switch_to.window(self.driver.window_handles[1])
-        self.driver.find_element(By.LINK_TEXT,'演讲申请').click()
+        self.driver.find_element(By.LINK_TEXT, '演讲申请').click()
 
     def teardown_method(self):
         time.sleep(10)
         self.driver.quit()
-
-
